b8cdc26d5a8a13d5b64bfae068ab2df4d39a4d2f
201702083 <201702083@o.cnu.ac.kr>
2022-07-05 17:49:44 +0900

    :sparkles: Simple WAS with stress api

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	Cpu "dockercicd/cpu"
+	Mem "dockercicd/mem"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Println("[log] someone connected")
-		w.Write([]byte("Hello World"))
+		w.Write([]byte("I wanna break pods\nGo to /memStress & /cpuStress"))
+	})
+	http.HandleFunc("/cpuStress",func(w http.ResponseWriter, req *http.Request){
+		Cpu.OccurCpuStress()
+		w.Write([]byte("cpu is burning"))
+	})
+	http.HandleFunc("/memStress", func(w http.ResponseWriter, req *http.Request){
+		Mem.OccurMemStress()
+		w.Write([]byte("mem is melting"))
 	})
 
 	http.ListenAndServe(":5000", nil)
